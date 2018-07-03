8c908946c0597240a1c2ab3fd071ab3cfc8acdb3
zackhitch <zackhitch@users.noreply.github.com>
2018-07-03 10:28:02 -0700

    added new component folder -WIP

---
+++ pen-pal/src/components/PenPalProfile/InfoCard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Penpal from '../Penpal';
+
+class InfoCard extends Component {
+  constructor() {
+    super();
+  }
+
+  // CDM
+}
+
+export default InfoCard;
