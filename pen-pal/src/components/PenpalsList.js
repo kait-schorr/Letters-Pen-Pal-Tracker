4fea4c41a48830e20706b32d2d8cc4434083b380
Kaitlyn Schorr <kaitlynschorr@gmail.com>
2018-07-03 01:45:47 -0500

    MERGE MASTER

--- pen-pal/src/components/PenpalsList.js
+++ pen-pal/src/components/PenpalsList.js
@@ -1,16 +1,11 @@
-import React from 'react';
-import Penpal from './Penpal';
-import './PenpalList.css';
+import React from "react";
+import Penpal from "./Penpal";
+import "./PenpalList.css";
 
 const PenpalsList = props => {
   return (
     <div className="friends-card">
-<<<<<<< HEAD
-      Penpal List
-      {/* {props.friends.map(friend => (
-=======
       {props.penpals.map(penpal => (
->>>>>>> 5fe38c62191b9ef2f94056c0666ff8600e7dc9a3
         <Penpal
           key={penpal.id}
           name={penpal.name}
@@ -18,7 +13,8 @@ const PenpalsList = props => {
           letter={penpal.letter}
           // picture={friend.picture}
         />
-      ))} */}
+      ))}{" "}
+      */}
     </div>
   );
 };
